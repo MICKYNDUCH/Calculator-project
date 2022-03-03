227373da2124d56099f3779fc7d71318afca4641
michael njenga <mickynduch@gmail.com>
2022-03-03 13:21:56 +0300

    add index.html

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var add = function(number1, number2) {
+  return number1 * number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
+
+
+
+var add = function(mike, njenga) {
+  return mike + njenga;
+};
+var mike =  prompt("Enter a name:");
+var njenga = prompt("Enter another name:");
+alert(add(mike,njenga));
