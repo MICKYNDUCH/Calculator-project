b095dff7b0b4319c4b3875f22c464bbb745d397c
michael njenga <mickynduch@gmail.com>
2022-03-12 12:44:31 +0300

    add modified index,html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,45 @@
 var add = function(number1, number2) {
+  return number1 + number2;
+};
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
   return number1 * number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
-
-
-
-var add = function(mike, njenga) {
-  return mike + njenga;
+var divide = function(number1, number2) {
+  return number1 / number2;
 };
-var mike =  prompt("Enter a name:");
-var njenga = prompt("Enter another name:");
-alert(add(mike,njenga));
+
+$(document).ready(function() {
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  alert(add(number1, number2));
+});
+
+// $(document).ready(function() {
+//  var number1 = parseInt(prompt("Enter a number:"));
+//  var number2 = parseInt(prompt("Enter another number:"));
+//  alert(add(number1, number2));
+// });
+
+// var add = function(number1 , number2) {
+// return number1 / number2 ;
+// }
+// var number1 = parseInt(prompt("Enter a number"));
+// var number2 = parseInt(prompt("Enter another number"));
+// alert("The answer is" + add(number1 , number2));
+//
+// var add = function(number1 , number2) {
+// return number1 + number2 ;
+// }
+// var number1 = parseInt(prompt("Enter a number"));
+// var number2 = parseInt(prompt("Enter another number"));
+// alert("The answer is" + add(number1 , number2));
+//
+// var add = function(number1 , number2) {
+// return number1 - number2 ;
+// }
+// var number1 = parseInt(prompt("Enter a number"));
+// var number2 = parseInt(prompt("Enter another number"));
+// alert("The answer is" + add(number1 , number2));
